ec0aad6fb83553528140c048cd9be911a6ad0042
Vinícius Campos <vscampos2k@gmail.com>
2025-07-17 23:12:11 -0300

    feat: add BookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface quickSearchOption {
+  imagemUrl: string
+  title: string
+}
+
+export const quickSearchOptions: quickSearchOption[] = [
+  {
+    imagemUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imagemUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imagemUrl: "/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imagemUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imagemUrl: "/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imagemUrl: "/hidratacao.svg",
+    title: "Hidratação",
+  },
+]
